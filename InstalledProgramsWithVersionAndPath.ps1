d39ce8e621ea13e77d430091ad635c14732157ae
SmolBlackHole <cgoerlac@gmail.com>
2023-03-16 12:22:36 +0100

    Create InstalledProgramsWithVersionAndPath.ps1

---
+++ InstalledProgramsWithVersionAndPath.ps1
@@ -0,0 +1,8 @@
+﻿$programs = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* |
+            Select-Object DisplayName, DisplayVersion, InstallLocation |
+            Where-Object { $_.DisplayName -ne $null } |
+            ConvertTo-Json -Depth 100
+
+$desktop = [Environment]::GetFolderPath("Desktop")
+$filename = Join-Path $desktop "Programme.json"
+$programs | Out-File -Encoding utf8 -FilePath $filename
